8e7df6978f9bb8f9204e4c163a7e8b80b75efe2b
mleelaw <mleelaw123@gmail.com>
2025-03-09 18:45:11 -0500

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/exomine_fullstack_tyler_mlee/client/",
+  base: "/exomine_fullstack_tyler_mlee/",
   server: {
     port: 5173,
     proxy: {
